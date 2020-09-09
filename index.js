150c8b41071974e396f115248a11534ca9144cd2
Nkowne63 <neutron63zf@gmail.com>
2020-09-09 17:08:11 +0900

    add: finalization sample

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("test");
+
+// heldValueにはregisterの第2引数が渡る
+const registry = new FinalizationRegistry((heldValue) => {
+  console.log("GC happened", heldValue);
+});
+
+let a = { b: { some: "object" } };
+
+// 前半は弱参照、後半は強参照なのでこれは不正なコードになる
+// registry.register(a, { ref: a });
+
+registry.register(a.b, "a.b");
+
+// registry.unregister(a);
+
+a = null;
+
+// Perform GC by clicking trash can icon in Peformance Tab in Chrome Devtools
